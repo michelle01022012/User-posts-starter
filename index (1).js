f7fb995f6ee000d88616de344a214a7713f1e3e3
Michelle Anderson <manderson0102@yahoo.com>
2026-02-26 03:43:36 -0500

    updates

--- index (1).js
+++ index (1).js
@@ -2,18 +2,16 @@
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
 async function main() {
-    const users= await fetch("https://jsonplaceholder.tyicode.com/users");
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
     const usersData = await users.json();
     const userListEl = document.querySelector(".user-list");
-
     userListEl.innerHTML = usersData
-    usersData
       .map(
          (user) => 
          `<div class="user-card">
             <div class="user-card__container">
               <h3>${user.name}</h4>
-                <p><b>Email$:</b> ${user.email}</p>
+                <p><b>Email:</b> ${user.email}</p>
                 <p><b>Phone:</b> ${user.phone}</p>
                 <p><b>Website:</b> <a href="${user.website}" target="_blank">
                 ${user.website}

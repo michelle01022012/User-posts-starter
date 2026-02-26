6cf942863104033c6877d5dcde2c675c7c0cff12
Michelle Anderson <manderson0102@yahoo.com>
2026-02-26 09:44:08 -0500

    changes

--- index (1).js
+++ index (1).js
@@ -9,14 +9,19 @@ async function main() {
 }  
     main();
 
+    function showUserPosts(id) {
+        window.location.href = `${window.location.origin}/user.html`
+    }
+
     function userHTML(user) {
-        return `<div class="user-card">
-       <div class="user-card__container">
-              <h3>${user.name}</h4>
-                <p><b>Email:</b> ${user.email}</p>
-                <p><b>Phone:</b> ${user.phone}</p>
-                <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
-                ${user.website}
-                </a></p>
-            </div>
-          </div>`;
+        return `<div class="user-card" onclick="showUserPosts(${user.id})'
+    }      <div class="user-card__container">
+            <h3>${user.name}</h4>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+            ${user.website}
+            </a></p>
+       </div>
+    </div>`;
+    }

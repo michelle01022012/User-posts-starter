5405490ac74a786510b6fd63b9c06186963e5c12
Michelle Anderson <manderson0102@yahoo.com>
2026-02-27 19:02:32 -0500

    updates

--- user (1).js
+++ user (1).js
@@ -1,1 +1,16 @@
-localStorage.getItem("id");
+async function main() {
+    const id = localStorage.getItem("id")
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postsData = await posts.json();
+
+    postsData.map(post => `
+        <div class="post">
+         <div class="post__title">
+          Post Title
+         </div>
+         </P>
+        </div>
+    `)
+}
+
+main();
